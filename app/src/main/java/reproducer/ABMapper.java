884d4bec52cbfc6b8b28c9d8d8f7c8464cdad00e
Dmitriy Kosolobov <2144933+SaltOfTheFlame@users.noreply.github.com>
2024-10-24 15:04:26 +0300

    Update ABMapper.java
    try the fix

--- app/src/main/java/reproducer/ABMapper.java
+++ app/src/main/java/reproducer/ABMapper.java
@@ -3,7 +3,7 @@ package reproducer;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(builder = @Mapper.Builder(disableBuilder = true))
 interface ABMapper {
     @Mapping(target = "id", constant = "321")
     B map(A a);
